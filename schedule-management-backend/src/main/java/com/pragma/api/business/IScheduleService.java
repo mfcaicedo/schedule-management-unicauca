56192dcece76f7099f774d9282722c6b5df437c0
mfcaicedo <mfcaicedo@unicauca.edu.co>
2023-03-18 00:05:24 -0500

    Grupo 2. configuracion cambiada

--- schedule-management-backend/src/main/java/com/pragma/api/business/IScheduleService.java
+++ schedule-management-backend/src/main/java/com/pragma/api/business/IScheduleService.java
@@ -6,7 +6,11 @@ import com.pragma.api.domain.ScheduleResponseDTO;
 import java.util.List;
 
 public interface IScheduleService {
-
+    /**
+     * Metodo para guardar una franja horaria
+     * @param save objeto con la informacion de la franja horaria
+     * @return objeto con la informacion de la franja horaria guardada
+     */
     ScheduleResponseDTO saveSchedule(ScheduleRequestDTO save);
     ScheduleResponseDTO updateSchedule(Long code, ScheduleRequestDTO update);
     Boolean deleteSchedule(Long code);
