17e55fc0df581649f148aafdfefb278a0929c4f4
mfcaicedo <mfcaicedo@unicauca.edu.co>
2023-04-05 13:30:54 -0500

    Primera parte de iconos actualizados, colores en el modulo de oferta academica actualizados, cambio del logo de unicauca

--- schedule-management-frontend/src/app/containers/default-layout/_nav.ts
+++ schedule-management-frontend/src/app/containers/default-layout/_nav.ts
@@ -27,9 +27,9 @@ export const navItems: INavData[] = [
         iconComponent: { name: 'cilCloudUpload' }
       }, 
       {
-        name: 'Editar oferta',
+        name: 'Descargar oferta',
         url: '/schedule/view-oa',
-        iconComponent: { name: 'cilPen' }
+        iconComponent: { name: 'cilCloudDownload' }
       }
     ]
   },
@@ -39,19 +39,20 @@ export const navItems: INavData[] = [
     iconComponent:{name:'cilRoom'},
     children:[
       {
-        name: 'Ver ambientes',
-        url: '/environment/all', 
-        iconComponent: { name: 'cilMediaPlay' }
-      },
-      {
         name:'Crear ambiente',
         url:'/environment/create', 
         iconComponent: { name: 'cilPlus' }
       },
-      // {
-      //   name:'Edit environment',
-      //   url:'/environment/edit/1'
-      // }
+      {
+        name:'Cargar ambientes',
+        url:'/environment/create', //se debe cambiar por la ruta de carga de ambientes
+        iconComponent: { name: 'cilCloudUpload' }
+      },
+      {
+        name: 'Ver ambientes',
+        url: '/environment/all', 
+        iconComponent: { name: 'cilMagnifyingGlass' }
+      },
     ]
   },
   {
@@ -60,20 +61,16 @@ export const navItems: INavData[] = [
     iconComponent:{name:'cilDevices'},
     children:[
       {
-        name:'Ver recursos',
-        url:'/resource/all', 
-        // iconComponent: { name: 'cilMagnifyingGlass' }
-        iconComponent: { name: 'cilMediaPlay' }
-      },
-      {
         name:'Crear recurso',
         url:'/resource/create', 
         iconComponent: { name: 'cilPlus' }
       },
-      // {
-      //   name:'Edit resource',
-      //   url:'/resource/edit/1'
-      // }
+      {
+        name:'Ver recursos',
+        url:'/resource/all', 
+        iconComponent: { name: 'cilMagnifyingGlass' }
+        // iconComponent: { name: 'cilMediaPlay' }
+      },
     ]
   },
   {
@@ -95,216 +92,4 @@ export const navItems: INavData[] = [
       }
     ]
   },
-  // {
-  //   name: 'Base',
-  //   url: '/base',
-  //   iconComponent: { name: 'cil-puzzle' },
-  //   children: [
-  //     {
-  //       name: 'Accordion',
-  //       url: '/base/accordion'
-  //     },
-  //     {
-  //       name: 'Breadcrumbs',
-  //       url: '/base/breadcrumbs'
-  //     },
-  //     {
-  //       name: 'Cards',
-  //       url: '/base/cards'
-  //     },
-  //     {
-  //       name: 'Carousel',
-  //       url: '/base/carousel'
-  //     },
-  //     {
-  //       name: 'Collapse',
-  //       url: '/base/collapse'
-  //     },
-  //     {
-  //       name: 'List Group',
-  //       url: '/base/list-group'
-  //     },
-  //     {
-  //       name: 'Navs & Tabs',
-  //       url: '/base/navs'
-  //     },
-  //     {
-  //       name: 'Pagination',
-  //       url: '/base/pagination'
-  //     },
-  //     {
-  //       name: 'Placeholder',
-  //       url: '/base/placeholder'
-  //     },
-  //     {
-  //       name: 'Popovers',
-  //       url: '/base/popovers'
-  //     },
-  //     {
-  //       name: 'Progress',
-  //       url: '/base/progress'
-  //     },
-  //     {
-  //       name: 'Spinners',
-  //       url: '/base/spinners'
-  //     },
-  //     {
-  //       name: 'Tables',
-  //       url: '/base/tables'
-  //     },
-  //     {
-  //       name: 'Tabs',
-  //       url: '/base/tabs'
-  //     },
-  //     {
-  //       name: 'Tooltips',
-  //       url: '/base/tooltips'
-  //     }
-  //   ]
-  // },
-  // {
-  //   name: 'Buttons',
-  //   url: '/buttons',
-  //   iconComponent: { name: 'cil-cursor' },
-  //   children: [
-  //     {
-  //       name: 'Buttons',
-  //       url: '/buttons/buttons'
-  //     },
-  //     {
-  //       name: 'Button groups',
-  //       url: '/buttons/button-groups'
-  //     },
-  //     {
-  //       name: 'Dropdowns',
-  //       url: '/buttons/dropdowns'
-  //     },
-  //   ]
-  // },
-  // {
-  //   name: 'Forms',
-  //   url: '/forms',
-  //   iconComponent: { name: 'cil-notes' },
-  //   children: [
-  //     {
-  //       name: 'Form Control',
-  //       url: '/forms/form-control'
-  //     },
-  //     {
-  //       name: 'Select',
-  //       url: '/forms/select'
-  //     },
-  //     {
-  //       name: 'Checks & Radios',
-  //       url: '/forms/checks-radios'
-  //     },
-  //     {
-  //       name: 'Range',
-  //       url: '/forms/range'
-  //     },
-  //     {
-  //       name: 'Input Group',
-  //       url: '/forms/input-group'
-  //     },
-  //     {
-  //       name: 'Floating Labels',
-  //       url: '/forms/floating-labels'
-  //     },
-  //     {
-  //       name: 'Layout',
-  //       url: '/forms/layout'
-  //     },
-  //     {
-  //       name: 'Validation',
-  //       url: '/forms/validation'
-  //     }
-  //   ]
-  // },
-  // {
-  //   name: 'Charts',
-  //   url: '/charts',
-  //   iconComponent: { name: 'cil-chart-pie' }
-  // },
-  // {
-  //   name: 'Icons',
-  //   iconComponent: { name: 'cil-star' },
-  //   url: '/icons',
-  //   children: [
-  //     {
-  //       name: 'CoreUI Free',
-  //       url: '/icons/coreui-icons',
-  //       badge: {
-  //         color: 'success',
-  //         text: 'FREE'
-  //       }
-  //     },
-  //     {
-  //       name: 'CoreUI Flags',
-  //       url: '/icons/flags'
-  //     },
-  //     {
-  //       name: 'CoreUI Brands',
-  //       url: '/icons/brands'
-  //     }
-  //   ]
-  // },
-  // {
-  //   name: 'Notifications',
-  //   url: '/notifications',
-  //   iconComponent: { name: 'cil-bell' },
-  //   children: [
-  //     {
-  //       name: 'Alerts',
-  //       url: '/notifications/alerts'
-  //     },
-  //     {
-  //       name: 'Badges',
-  //       url: '/notifications/badges'
-  //     },
-  //     {
-  //       name: 'Modal',
-  //       url: '/notifications/modal'
-  //     },
-  //     {
-  //       name: 'Toast',
-  //       url: '/notifications/toasts'
-  //     }
-  //   ]
-  // },
-  // {
-  //   name: 'Widgets',
-  //   url: '/widgets',
-  //   iconComponent: { name: 'cil-calculator' },
-  //   badge: {
-  //     color: 'info',
-  //     text: 'NEW'
-  //   }
-  // },
-  // {
-  //   title: true,
-  //   name: 'Extras'
-  // },
-  // {
-  //   name: 'Pages',
-  //   url: '/login',
-  //   iconComponent: { name: 'cil-star' },
-  //   children: [
-  //     {
-  //       name: 'Login',
-  //       url: '/login'
-  //     },
-  //     {
-  //       name: 'Register',
-  //       url: '/register'
-  //     },
-  //     {
-  //       name: 'Error 404',
-  //       url: '/404'
-  //     },
-  //     {
-  //       name: 'Error 500',
-  //       url: '/500'
-  //     }
-  //   ]
-  // },
 ];
