bb8a6493283f9b21c830e224bbc55783f5b3c5e1
julian andres niño niño <jnino@unicauca.edu.co>
2023-05-01 17:37:07 -0500

    Se creo el crud para gestionar eventos y se realizaron pruebas

--- schedule-management-backend/src/main/java/com/pragma/api/model/Event.java
+++ schedule-management-backend/src/main/java/com/pragma/api/model/Event.java
@@ -4,6 +4,8 @@ import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -11,6 +13,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+
+import com.pragma.api.model.enums.EventTypeEnumeration;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -27,12 +31,12 @@ public class Event {
     @Id()
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "event_id")
-    private Integer id;
+    private Integer eventId;
 
     @Column(name = "event_name", nullable = false)
-    private String NombreEvento;
+    private String eventName;
     @Column(name = "event_manager_name", nullable = false)
-    private String NombreEncargado;
+    private String eventManagerName;
     @Column(name = "description")
     private String description;
 
@@ -46,5 +50,9 @@ public class Event {
 
     @OneToMany(mappedBy = "event")
     private Set<Schedule> schedules;
+
+    @Column(name = "event_type",nullable = false)
+    @Enumerated(EnumType.STRING)
+    private EventTypeEnumeration eventType;
     
 }
