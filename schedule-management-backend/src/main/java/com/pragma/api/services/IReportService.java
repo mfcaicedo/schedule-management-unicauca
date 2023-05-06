76ee4dbcc54e6ae2b933fe82c9455ef7c69a8c3a
AndreaC2000 <andreacuatindioy226@gmail.com>
2023-05-05 20:25:28 -0500

    tercer commit backend

--- schedule-management-backend/src/main/java/com/pragma/api/services/IReportService.java
+++ schedule-management-backend/src/main/java/com/pragma/api/services/IReportService.java
@@ -7,13 +7,9 @@ import java.util.List;
 
 public interface IReportService {
     
-<<<<<<< HEAD
-    
-=======
     List<EnvironmentDTO> findAllEnvironment();
 
     EnvironmentDTO findById(String id);
->>>>>>> a7aed25d3f9355a5f906d1f84639e02c9421d368
 }
 
 
