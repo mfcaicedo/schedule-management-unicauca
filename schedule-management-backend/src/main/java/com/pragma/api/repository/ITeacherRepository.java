0cfc5bc90c33f89c80c8f59fc2ef169416ecd24f
Alvaro Erazo <ader148@hotmail.com>
2023-05-10 22:18:39 -0500

    mezcla de rama y funcion de cargar profesores por archivo

--- schedule-management-backend/src/main/java/com/pragma/api/repository/ITeacherRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pragma.api.repository;
-
-import com.pragma.api.model.Teacher;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ITeacherRepository extends JpaRepository<Teacher, String> {
-}
