568a22cf4f7f4fbf04a17b911cae2fb2a7414fe6
Brandon Nicolas Bohorquez Muñoz <99350462+bbohorquez08@users.noreply.github.com>
2023-05-13 17:41:27 -0500

    proceso plantilla v1

--- schedule-management-backend/src/main/java/com/pragma/api/controllers/AcademicOfferController.java
+++ schedule-management-backend/src/main/java/com/pragma/api/controllers/AcademicOfferController.java
@@ -1,15 +1,18 @@
 package com.pragma.api.controllers;
 
+import com.pragma.api.domain.Response;
 import com.pragma.api.domain.TemplateFileDTO;
 import com.pragma.api.services.IFileAcademicOffer;
 import com.pragma.api.services.ITemplateFileService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.List;
 
 @RestController
@@ -28,7 +31,6 @@ public class AcademicOfferController {
     }
 
 
-
     @PostMapping("/uploadFile")
     ResponseEntity<List<String>> uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
         return new ResponseEntity<>(this.iFileAcademicOffer.uploadFile(file), HttpStatus.OK);
@@ -39,5 +41,9 @@ public class AcademicOfferController {
         return new ResponseEntity<>(this.iTemplateFileService.uploadTemplateFile(file),HttpStatus.OK);
     }
 
+    @GetMapping("/downloadTemplate")
+    String downloadTemplate() throws IOException {
+        return this.iTemplateFileService.processFile();
+    }
 
 }
--- schedule-management-backend/src/main/java/com/pragma/api/services/FileTemplateImpl.java
+++ schedule-management-backend/src/main/java/com/pragma/api/services/FileTemplateImpl.java
@@ -1,18 +1,32 @@
 package com.pragma.api.services;
 
+import com.pragma.api.domain.Response;
 import com.pragma.api.domain.TemplateFileDTO;
 import com.pragma.api.model.Subject;
 import com.pragma.api.model.TemplateFile;
 import com.pragma.api.repository.ITemplateFileRepository;
-import org.apache.poi.ss.usermodel.Workbook;
+import com.pragma.api.util.file.templateclasses.FileRowEnvironment;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.hibernate.tool.schema.internal.exec.ScriptTargetOutputToFile;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -23,6 +37,9 @@ public class FileTemplateImpl implements ITemplateFileService{
 
     @Autowired
     private ModelMapper modelMapper;
+
+    @Autowired
+    private ResourceLoader resourceLoader;
 
     @Autowired
     public FileTemplateImpl(ITemplateFileRepository templateFileRepository) {
@@ -44,4 +61,49 @@ public class FileTemplateImpl implements ITemplateFileService{
 
         return modelMapper.map(this.templateFileRepository.save(templateFile),TemplateFileDTO.class);
     }
+
+    @Override
+    public String processFile() throws IOException {
+        String name= "Plantilla_oferta_academica.xlsm";
+
+        final String pathLocal= "C:/Users/Personal/Desktop/ING SISTEMAS/SEMESTRE 8/PROYECTO 1/Proyecto/schedule-management-unicauca/schedule-management-backend/src/main/resources/files/templates/Plantilla_oferta_academica.xlsm";
+
+
+
+        String prueba = obtenerRutaArchivo(name);
+        prueba.replace("\\","/");
+
+        Workbook workbook = WorkbookFactory.create(new File(prueba));
+
+        Sheet sheet = workbook.getSheetAt(0);
+
+
+        Row row = sheet.getRow(1);
+        System.out.println("dato: "+row.getCell(1));
+
+        workbook.close();
+
+        return null;
+
+
+    }
+
+    private String obtenerRutaArchivo(String nombreArchivo) {
+        try {
+            Resource resource = resourceLoader.getResource("file:" + nombreArchivo);
+            File file = resource.getFile();
+            return file.getAbsolutePath();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+
+
 }
+
+
+
+
