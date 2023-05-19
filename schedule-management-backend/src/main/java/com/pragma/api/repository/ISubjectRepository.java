c00c7ea41675563a1f5b4f31c00afa46bfd85d91
ererazo <ererazo@unicauca.edu.co>
2023-05-18 19:20:18 -0500

    Se implementa validacion de tipo de datos y propias del proceso, se modifica la clase ResponseFile y se implementa guardado de materias en la BD

--- schedule-management-backend/src/main/java/com/pragma/api/repository/ISubjectRepository.java
+++ schedule-management-backend/src/main/java/com/pragma/api/repository/ISubjectRepository.java
@@ -2,10 +2,18 @@ package com.pragma.api.repository;
 
 import com.pragma.api.model.Subject;
 import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.Query;
+//import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
 /**
  * Repository de Spring para las operaciones CRUD sobre la tabla SUBJECT.
  */
 @Repository
-public interface ISubjectRepository extends JpaRepository<Subject,String> {
+public interface ISubjectRepository extends JpaRepository<Subject, String> {
+
+    boolean existsByNameAndProgram_ProgramId(String name, String programId);
+
+//    @Query("SELECT CASE WHEN COUNT(s.subject_code) > 0 THEN true ELSE false END FROM subject s WHERE UPPER(s.name) LIKE UPPER(:name) AND UPPER(s.program_id) LIKE UPPER(:program_id)")
+//    boolean existsByNameAndProgram(@Param("name") String name, @Param("program_id") String program_id);
 }
