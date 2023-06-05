d7b3e761c80bef43820b29894968b1dd4cc3fd81
William Caicedo <wcaicedom@unicauca.edu.co>
2023-06-05 14:48:46 -0500

    Se sube codigo que funciona para realizar el reporte de Facultad/Programa que corresponde al mockup

--- schedule-management-backend/src/main/java/com/pragma/api/domain/ReportDTO.java
+++ schedule-management-backend/src/main/java/com/pragma/api/domain/ReportDTO.java
@@ -27,9 +27,13 @@ public class ReportDTO {
     private String subjectName;
     private String programName;
     private String programColor;
+    private String courseGroup;
+    private String courseDescription;
+    private int subjectSemester;
 
     // Agrega los demás campos correspondientes a las otras tablas
 
+    //Constructor para hacer reporte por salon
     public ReportDTO(Long id,DaysEnumeration day, LocalTime startingTime, LocalTime endingTime, Date startingDate, Date endingDate,String environmentName, String subjectName, String programName,String programColor) {
         this.id=id;
         this.day = day;
@@ -41,6 +45,23 @@ public class ReportDTO {
         this.subjectName = subjectName;
         this.programName = programName;
         this.programColor=programColor;
+    }
+
+    //Constructor para hacer reporte por Facultad/programa
+    public ReportDTO(Long id, DaysEnumeration day, LocalTime startingTime, LocalTime endingTime, Date startingDate, Date endingDate, String subjectName, int subjectSemester, String courseGroup, String courseDescription, String programName, String programColor, String environmentName) {
+        this.id = id;
+        this.day = day;
+        this.startingTime = startingTime;
+        this.endingTime = endingTime;
+        this.startingDate = startingDate;
+        this.endingDate = endingDate;
+        this.subjectName = subjectName;
+        this.subjectSemester = subjectSemester;
+        this.courseGroup = courseGroup;
+        this.courseDescription = courseDescription;
+        this.programName = programName;
+        this.programColor = programColor;
+        this.environmentName = environmentName;
     }
 
 }
--- schedule-management-backend/src/main/java/com/pragma/api/repository/IReportRepository.java
+++ schedule-management-backend/src/main/java/com/pragma/api/repository/IReportRepository.java
@@ -22,8 +22,21 @@ public interface IReportRepository extends JpaRepository<Report, Long> {
      " ON e.environment_id= s.environment_id INNER JOIN course c ON c.course_id=s.course_id INNER JOIN "+
      " subject su ON su.subject_code= c.subject_code INNER JOIN program p ON p.program_id=su.program_id "+
      " WHERE s.environment_id= :environment_id", nativeQuery = true)
-     //List<Report> getCombinedDataScheduleByEnvironmentId(@Param("environment_id") Integer environment_id);
 
      List<Object[]> getCombinedDataScheduleByEnvironmentId(@Param("environment_id") Integer environment_id);
+
+     
+     //Metodo para traer los datos necesarios para realizar el reporte facultad/programa por programa id ordenado
+     //ascendentemente por el campo semestre de materia
+     @Query( value=" SELECT s.id,s.day,s.starting_time,s.ending_time,s.starting_Date,s.ending_Date, su.name as materiaName, "+
+                   " su.semester, c.course_group,c.description,pro.name as programaName,pro.color,e.name as ambienteName FROM SCHEDULE AS s "+
+                   " INNER JOIN course AS c on s.course_id=c.course_id INNER JOIN subject AS su on "+
+                   " su.subject_code=c.subject_code INNER JOIN environment AS e ON e.environment_id=s.environment_id"+
+                   " INNER JOIN program AS pro on pro.program_id=su.program_id WHERE pro.program_id= :program_id"+
+                   " ORDER BY su.semester ASC", nativeQuery = true)
+
+    List<Object[]> getCombinedDataScheduleByProgramId(@Param("program_id") String program_id);
+
+
 
 }
