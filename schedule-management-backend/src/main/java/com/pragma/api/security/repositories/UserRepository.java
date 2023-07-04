e4e9f9960ca34ea87ffab48316d32f4392ec6e61
whastica <drlook92@gmail.com>
2023-07-03 21:08:52 -0500

    metodo cambiar contraseña

--- schedule-management-backend/src/main/java/com/pragma/api/security/repositories/UserRepository.java
+++ schedule-management-backend/src/main/java/com/pragma/api/security/repositories/UserRepository.java
@@ -8,6 +8,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUsername(String username);
     Optional<User> findByUsernameOrEmail(String username, String email);
+    Optional<User> findByTokenPassword(String TokenPassword);
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
 
