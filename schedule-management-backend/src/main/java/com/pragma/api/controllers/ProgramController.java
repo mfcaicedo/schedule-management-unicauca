7bd6a5880c775bf8322aaa3d3607bd6b85770035
William Caicedo <wcaicedom@unicauca.edu.co>
2023-07-05 02:11:47 -0500

    Actualizacion consulta de programas, para hacer las convenciones de los reportes

--- schedule-management-backend/src/main/java/com/pragma/api/controllers/ProgramController.java
+++ schedule-management-backend/src/main/java/com/pragma/api/controllers/ProgramController.java
@@ -40,4 +40,10 @@ public class ProgramController {
         return this.iProgramService.findAllProgramByFacultyId(id);
     }
 
+    //Metodo para obtener todos los programas para ser listados
+    @GetMapping("/consultarProgramas/")
+    public Response<List<ProgramDTO>> consultarTodosProgramas() {
+        return this.iProgramService.findAllProgramNameColor();
+    }
+
 }
